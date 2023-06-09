5d15ad6d98caf5fd003968ad386e89b0509e5b0a
Max <max.aumard93@gmail.com>
2023-06-09 21:09:22 +0200

    commentaire ia + supression des affichages inutiles

--- src/ia.py
+++ src/ia.py
@@ -3,58 +3,47 @@ import numpy as np
 from tensorflow.keras import layers, Sequential
 import matplotlib.pyplot as plt
 
-# Chargement des données d'entraînement
+# Chargement des données
 train_data = pd.read_csv('https://maxime-devanne.com/datasets/ECG200/ECG200_TRAIN.tsv', sep='\t', header=None)
-# Chargement des données de test
 test_data = pd.read_csv('https://maxime-devanne.com/datasets/ECG200/ECG200_TEST.tsv', sep='\t', header=None)
 
-# Extraction des caractéristiques d'entraînement
-x_train = train_data.iloc[:, 1:].values
-# Extraction des étiquettes de classe d'entraînement
-y_train = train_data.iloc[:, 0].values
-
-# Extraction des caractéristiques de test
-x_test = test_data.iloc[:, 1:].values
-# Extraction des étiquettes de classe de test
-y_test = test_data.iloc[:, 0].values
-
-# Suppression et recuperation de la première colonne des données d'entraînement et de test
 train_labels = train_data.pop(0)
 test_labels = test_data.pop(0)
 
-# Conversion des étiquettes en binaire (0 ou 1)
-y_train = (y_train + 1) / 2
-y_test = (y_test + 1) / 2
+# Encodage one hote
+train_labels = tf.keras.utils.to_categorical(train_labels, num_classes=2)
+test_labels = tf.keras.utils.to_categorical(test_labels, num_classes=2)
 
 # Modèle de réseau neuronal convolutionnel (CNN)
 model_cnn = Sequential([
-    layers.Conv1D(64, 3, activation='relu', input_shape=(x_train.shape[1], 1)),
+    layers.Conv1D(64, 3, activation='relu', input_shape=(train_data.shape[1], 1)),
     layers.MaxPooling1D(2),
-    layers.Conv1D(64, 3, activation='relu', input_shape=(x_train.shape[1], 1)),
+    layers.Conv1D(64, 3, activation='relu'),
     layers.MaxPooling1D(2),
     layers.Flatten(),
     layers.Dense(64, activation='relu'),
-    layers.Dense(1, activation='sigmoid')
+    layers.Dropout(0.5),
+    layers.Dense(2, activation='softmax')
 ])
-
-# Compilation du modèle CNN
-model_cnn.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-# Entraînement du modèle CNN sur les données d'entraînement sur 50 époques
-model_cnn.fit(x_train, y_train, epochs=50, validation_data=(x_test, y_test))
 
 # Modèle de réseau neuronal récurrent (RNN)
 model_rnn = Sequential([
-    layers.LSTM(units=64, return_sequences=True, input_shape=(x_train.shape[1], 1)),
-    layers.MaxPooling1D(2),
-    layers.Flatten(),
+    layers.LSTM(units=64, return_sequences=True, input_shape=(train_data.shape[1], 1)),
+    layers.Dropout(0.5),
+    layers.LSTM(units=64),
     layers.Dense(64, activation='relu'),
-    layers.Dense(1, activation='sigmoid')
+    layers.Dropout(0.5),
+    layers.Dense(2, activation='softmax')
 ])
 
-# Compilation du modèle RNN
-model_rnn.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-# Entraînement du modèle RNN avec 50 époques
-model_rnn.fit(x_train, y_train, epochs=50, validation_data=(x_test, y_test))
+# Compilation
+model_cnn.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model_rnn.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Entrainement
+history_cnn = model_cnn.fit(train_data, train_labels, epochs=10, validation_data=(test_data, test_labels))
+history_rnn = model_rnn.fit(train_data, train_labels, epochs=10, validation_data=(test_data, test_labels))
+
 
 # Sauvegarde du modèle CNN dans un fichier h5
 model_cnn.save('../models/model_cnn.h5')
